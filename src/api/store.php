4ba2aba9e96c97aa4bab23a99f05c893e8b05389
istaqom <istaqom@gmail.com>
2022-12-06 10:55:19 +0800

    update store & store_menu

--- src/api/store.php
+++ src/api/store.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/store', function (Request $request, Response $response, $args) {
-    $sql = "SELECT *  from store";
+    $sql = "CALL show_store()";
 
     try {
         $db = new DB();
